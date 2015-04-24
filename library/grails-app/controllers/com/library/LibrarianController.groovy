058fac7102bce38d68fea502ec35bca0b077d667
Webarchitecture <b3024928@my.shu.ac.uk>
2015-04-24 15:02:37 +0100

    trying to set up login/logout facilities for librarian

--- library/grails-app/controllers/com/library/LibrarianController.groovy
+++ library/grails-app/controllers/com/library/LibrarianController.groovy
@@ -81,6 +81,30 @@ class LibrarianController {
             return
         }
 
+     def login() {
+        if (params.cName)
+        return [cName:params.cName, aName:params.aName]
+        }
+
+     def logout = {
+        session.user = null
+        redirect(uri:'/')
+        }
+     def validate() {
+        def user = Leader.findByUserName(params.username)
+          if (user && user.password == params.password){
+        session.user = user
+          if (params.cName)
+        redirect controller:params.cName, action:params.aName
+        else
+        redirect controller:'course', action:'index'
+        } else{
+        flash.message = "Invalid username and password."
+        render view:'login'
+        }
+      }
+
+
         librarianInstance.delete flush:true
 
         request.withFormat {
